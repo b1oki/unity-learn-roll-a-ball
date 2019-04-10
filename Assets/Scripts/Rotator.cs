d7e052658e895d95770db70a73aada1591f677f1
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-04-11 04:02:58 +0500

    Feat: first collectible

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private readonly float RotationSpeed = 15;
+
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * RotationSpeed * new Vector3(0, 0, 15));
+    }
+}
