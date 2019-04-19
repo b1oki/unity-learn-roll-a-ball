3f8ac8f3a2804876a58d876116b8dcf3292d95b0
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-04-19 23:25:18 +0500

    Feat: Player can jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float maxJumpHeight;
 
     private Rigidbody _rb;
 
@@ -13,11 +14,19 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        PlayerControl();
+    }
+
+    private void PlayerControl()
+    {
+        var jump = Input.GetAxis("Jump");
         var moveHorizontal = Input.GetAxis("Horizontal");
         var moveVertical = Input.GetAxis("Vertical");
-
         var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+        if (jump > 0 && _rb.position.y < maxJumpHeight)
+        {
+            movement.y = 2.0f;
+        }
         _rb.AddForce(movement * speed);
     }
 }
