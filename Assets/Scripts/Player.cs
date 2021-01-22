c640df85e6b73f296fc119f7204e48227036d61d
Alexander Sobyanin <git@alexandersobyanin.ru>
2021-01-22 07:26:30 +0500

    Feat: Unity 2020.2.2f1; New Input System; Gamepad support;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -6,20 +7,45 @@ public class Player : MonoBehaviour
     public float speed;
     public float maxJumpHeight;
     public Text scoresText;
+    public InputActionMap ballActions;
 
     private Rigidbody _rb;
+    private Vector3 _movement;
+    private Vector2 _actionValueMove;
+    private bool _actionValueJump;
+    private float _initialPlayerVerticalPosition;
     private int _scoresCount;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _initialPlayerVerticalPosition = _rb.position.y;
         _scoresCount = 0;
         SetScoresText();
+    }
+
+    private void OnEnable()
+    {
+        ballActions.Enable();
+        ballActions.FindAction("Move").performed += HandleMove;
+        ballActions.FindAction("Move").canceled += HandleCancelMove;
+        ballActions.FindAction("Jump").performed += HandleJump;
+        ballActions.FindAction("Jump").canceled += HandleCancelJump;
+    }
+
+    private void OnDisable()
+    {
+        ballActions.FindAction("Move").performed -= HandleMove;
+        ballActions.FindAction("Move").canceled -= HandleCancelMove;
+        ballActions.FindAction("Jump").performed -= HandleJump;
+        ballActions.FindAction("Jump").canceled -= HandleCancelJump;
+        ballActions.Disable();
     }
 
     private void FixedUpdate()
     {
         PlayerControl();
+        Debug.Log(_movement);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,18 +54,53 @@ public class Player : MonoBehaviour
         PickUpCollectible(other);
     }
 
+
+    private void HandleMove(InputAction.CallbackContext context)
+    {
+        _actionValueMove = context.ReadValue<Vector2>();
+    }
+
+    private void HandleCancelMove(InputAction.CallbackContext context)
+    {
+        _actionValueMove = context.ReadValue<Vector2>();
+    }
+
+    private void HandleJump(InputAction.CallbackContext context)
+    {
+        _actionValueJump = true;
+    }
+
+    private void HandleCancelJump(InputAction.CallbackContext context)
+    {
+        _actionValueJump = false;
+    }
+
     private void PlayerControl()
     {
-        var jump = Input.GetAxis("Jump");
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        if (jump > 0 && _rb.position.y < maxJumpHeight)
-        {
-            movement.y = 2.0f;
-        }
+        PlayerMove();
+        PlayerJump();
+        _rb.AddForce(_movement * speed);
+    }
 
-        _rb.AddForce(movement * speed);
+    private void PlayerMove()
+    {
+        _movement = new Vector3(_actionValueMove.x, 0.0f, _actionValueMove.y);
+    }
+
+    private void PlayerJump()
+    {
+        if (_actionValueJump)
+        {
+            if (_rb.position.y < maxJumpHeight)
+            {
+                _movement.y = 3.0f;
+            }
+        } else {
+            if (_rb.position.y > _initialPlayerVerticalPosition)
+            {
+                _movement.y = -1.0f;
+            }
+        }
     }
 
     private void PickUpCollectible(Component other)
